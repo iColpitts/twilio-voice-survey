9f289410b084afb5aebec3939395817a1059b9d8
icolpitts <izzieccampbell@gmail.com>
2020-05-10 17:55:45 -0400

    Add Artwork_local404 specific scripts and adjust logic for speaking them
    * added prompts and scripts
    * added logic to go from intro to prompts that can be recorded to hangup

--- server.js
+++ server.js
@@ -1,20 +1,97 @@
 const express = require('express');
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const fs = require('fs');
+const botSpeak = require('./bot_speak/scripts.json');
+const botPrompt = require('./bot_speak/prompts.json');
+const bodyParser = require('body-parser');
+
+const VoiceResponse = require('twilio').twiml.VoiceResponse
 
 const app = express();
 
-// Returns TwiML which prompts the caller to record a message
-app.post('/record', (request, response) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getPromptSection(array) {
+  const length = array.length;
+  return array[getRandomInt(length)];
+}
+
+function getPrompt() {
+  return getPromptSection(botPrompt.location) +
+    getPromptSection(botPrompt.situation)+
+    getPromptSection(botPrompt.ask);
+}
+
+// Record prompts when artists call in
+app.post('/welcome', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   const twiml = new VoiceResponse();
-  twiml.say('Hello. Please leave a message after the beep.');
+  //read scipts from .json
 
-  // Use <Record> to record the caller's message
+  const gather = twiml.gather({
+    numDigits: 1,
+    action: '/gather'
+  });
+
+  gather.say(botSpeak.hello + botSpeak.continue);
+
+  //if no response
+  twiml.say(botSpeak.bye);
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+app.post('/gather', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
+
+  // If the user entered digits, process their request
+  if (request.body.Digits) {
+    switch (request.body.Digits) {
+      case '1':
+        twiml.redirect('/prompt');
+        break;
+      case '2':
+        twiml.redirect('/bye');
+        break;
+      default:
+        twiml.say("Sorry, I don't understand that choice.");
+        twiml.redirect('/welcome');
+        break;
+    }
+  } else {
+    // If no input was sent, redirect to the /voice route
+    twiml.redirect('/bye');
+  }
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+app.post('/prompt', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
+  console.log(getPrompt());
+  twiml.say(getPrompt());
   twiml.record();
 
-  // End the call with <Hangup>
   twiml.hangup();
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+})
 
+app.post('/bye', (request,response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
+  twiml.say(botSpeak.bye);
+  twiml.hangup();
   // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
